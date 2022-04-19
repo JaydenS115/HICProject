55bf226ed1c784f3914380004de59862cd6b1e73
ColbyReinhart <colbyreinhart@gmail.com>
2022-04-19 15:51:21 -0400

    refactored main screen layout (now works by hiding elements instead of with anchors). Built main screen template.

---
+++ static/js/main.js
@@ -0,0 +1,11 @@
+
+function switchScreen(newScreen)
+{
+	// Hide all screens
+	(document.querySelectorAll(".screen")).forEach((screen) => {
+		screen.style.display = "none";
+	});
+
+	// Reveal the screen we want to look at
+	document.querySelector("#" + newScreen).style.display = "block";
+}

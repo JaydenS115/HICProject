2de880d0b81d116ae68edc3e29ed562b8243b235
ColbyReinhart <colbyreinhart@gmail.com>
2022-04-26 17:31:09 -0400

    Audio page fully functional

--- static/js/main.js
+++ static/js/main.js
@@ -32,5 +32,76 @@ carImage.addEventListener("click", (e) => {
 	soundMover.style.position = "absolute";
 	soundMover.style.left = x.toString() + "px";
 	soundMover.style.top = y.toString() + "px";
-	console.log(x, y);
+});
+
+// Toggleable buttons
+const toggleButtons = {
+	"hollowBookmark": "clickable fa-solid fa-bookmark solidBookmark toggleable",
+	"solidBookmark": "clickable fa-regular fa-bookmark hollowBookmark toggleable",
+	"pauseButton": "clickable fa-solid fa-play playButton toggleable",
+	"playButton": "clickable fa-solid fa-pause pauseButton toggleable"
+};
+document.querySelectorAll(".toggleable").forEach((e) => {
+	e.addEventListener("click", () => {
+		e.classList.forEach((className) => {
+			const classes = toggleButtons[className];
+			if (classes != undefined)
+			{
+				e.className = classes;
+			}
+		});
+	});
+});
+
+//
+// Media player
+//
+
+const songs = {
+	"amount": 3,
+	"songList": [
+		{"title": "Hey Jude", "artist": "The Beatles", "filename": "beatles.jpg"},
+		{"title": "Gangnam Style", "artist": "Psy", "filename": "gangnamstyle.webp"},
+		{"title": "Baby", "artist": "Justin Bieber", "filename": "justinbieber.jfif"}
+	]
+}
+
+const pathToFiles = "../static/images/";
+
+// Set the new song to play
+// Takes a songlist entry from songs as an argument
+function setNewSong(newSong)
+{
+	document.querySelectorAll(".stream").forEach((stream) => {
+		stream.querySelector(".songTitle").innerHTML = newSong.title;
+		stream.querySelector(".songArtist").innerHTML = newSong.artist;
+		stream.querySelector(".songFile").src = pathToFiles + newSong.filename;
+	});
+}
+
+let songIndex = 0;
+// Find a new song to play
+// Takes -1 if backwards, 1 if forwards
+// Returns the songList entry of songs corresponding to the new index
+function switchSong(whichWay)
+{
+	songIndex = (songIndex + whichWay) % songs.amount;
+	// js modulo is lame and doesn't work for negative numbers
+	if (songIndex == -1)
+	{
+		songIndex = songs.amount - 1;
+	}
+	return songs.songList[songIndex];
+}
+
+// Register song navigation events
+document.querySelectorAll(".songBackward").forEach((e) => {
+	e.addEventListener("click", () => {
+		setNewSong(switchSong(-1));
+	});
+});
+document.querySelectorAll(".songForward").forEach((e) => {
+	e.addEventListener("click", () => {
+		setNewSong(switchSong(1));
+	});
 });

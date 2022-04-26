ccbaf014b8a6375c60997844c832d54c6d316fcd
ColbyReinhart <colbyreinhart@gmail.com>
2022-04-26 16:36:08 -0400

    Added sound mover functionality

--- static/js/main.js
+++ static/js/main.js
@@ -10,11 +10,27 @@ function switchScreen(newScreen)
 	document.querySelector("#" + newScreen).style.display = "flex";
 }
 
+//
 // Do init stuff
-const clickSound = new Audio("../static/sounds/tap.mp3");
+//
 
+// Click sound effect
+const clickSound = new Audio("../static/sounds/tap.mp3");
 document.querySelectorAll(".clickable").forEach((e) => {
 	e.addEventListener("click", () => {
 		clickSound.play();
 	});
+});
+
+// Sound adjustor
+const soundLocatorBox = document.querySelector("#soundLocatorBox");
+const carImage = document.querySelector("#carImage");
+let soundMover = document.querySelector("#soundMover");
+carImage.addEventListener("click", (e) => {
+	const x = parseInt(e.clientX - soundLocatorBox.getBoundingClientRect().left - 15);
+	const y = parseInt(e.clientY - soundLocatorBox.getBoundingClientRect().top - 15);
+	soundMover.style.position = "absolute";
+	soundMover.style.left = x.toString() + "px";
+	soundMover.style.top = y.toString() + "px";
+	console.log(x, y);
 });

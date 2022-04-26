cf628497e2c12204beaa42aec91741e21264feef
ColbyReinhart <colbyreinhart@gmail.com>
2022-04-26 15:57:49 -0400

    Elements with the 'clickable' class now play a sound effect when clicked

--- static/js/main.js
+++ static/js/main.js
@@ -9,3 +9,12 @@ function switchScreen(newScreen)
 	// Reveal the screen we want to look at
 	document.querySelector("#" + newScreen).style.display = "flex";
 }
+
+// Do init stuff
+const clickSound = new Audio("../static/sounds/tap.mp3");
+
+document.querySelectorAll(".clickable").forEach((e) => {
+	e.addEventListener("click", () => {
+		clickSound.play();
+	});
+});
